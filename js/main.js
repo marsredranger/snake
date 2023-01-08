e64209af165a6588a0aa6e031f7fb9fbf2f4d125
marsredranger <redranges@protonmail.com>
2023-01-08 00:54:39 -0500

    add demo feature where computer plays snake with algorithm to avoid self.

--- js/main.js
+++ js/main.js
@@ -26,6 +26,7 @@ const NEW_GAME = "NEW_GAME";
 const PAUSED = "PAUSED";
 const PLAY = "PLAY";
 const GAME_OVER = "GAME_OVER";
+const DEMO = "DEMO";
 
 const APPLE_SIZE = 32;
 
@@ -35,6 +36,10 @@ const RANDOM_MIN_Y = 0;
 const RANDOM_MAX_Y = (HEIGHT - APPLE_SIZE) / APPLE_SIZE;
 
 const INTERVAL = 150;
+
+const SNAKE_COLOR = "#22ee22";
+const DEAD_SNAKE_COLOR = "#226edd";
+const APPLE_COLOR = "#de2323";
 
 const game = {
   status: NEW_GAME,
@@ -49,10 +54,7 @@ const game = {
     return true
   },
   appleEaten: function() {
-    if(snake.headCords[0] === apple.cords[0] && snake.headCords[1] === apple.cords[1]) {
-      return true;
-    }
-    return false;
+    return snake.headCords[0] === apple.cords[0] && snake.headCords[1] === apple.cords[1];
   },
   snakeCollision: function() {
     for(let cords of snake.cords.slice(1)){
@@ -66,7 +68,7 @@ const game = {
 
 const apple = {
   size: APPLE_SIZE,
-  color: "#de2323",
+  color: APPLE_COLOR,
   spawned: false,
   init: function() {
     this.cords = this.spawnApple();
@@ -84,7 +86,7 @@ const apple = {
 
 const snake = {
   init : function() {
-      this.color = "#22ee22";
+      this.color = SNAKE_COLOR;
       this.height = 32;
       this.width = 32;
       this.speed = SPEED;
@@ -154,7 +156,7 @@ const snake = {
   },
   kill: function() {
     this.speed = 0;
-    this.color = "#226edd";
+    this.color = DEAD_SNAKE_COLOR;
   },
   move: function(x, y) {
     this.cords.unshift([x, y]);
@@ -166,8 +168,121 @@ const snake = {
   }
 }
 
+let computerSnake = {
+  init: function() {
+    this.snake = snake;
+    this.snake.direction = UP;
+  },
+  getAppleCords: function() {
+    this.appleCords = apple.cords;
+  },
+
+
+  nextMove: function() {
+    // need to move up
+    if (this.snake.headCords[1] > this.appleCords[1]) {
+      console.log("COMPUTER SNAKE UP");
+      if (this.snake.direction !== DOWN && !this.wouldCollideUp(this.snake.headCords)) {
+        return UP;
+      } else if (!this.wouldCollideRight(this.snake.headCords)) {
+        return RIGHT;
+      } else if (!this.wouldCollideLeft(this.snake.headCords)) {
+        return LEFT;
+      }
+      // need to move left
+    } else if (this.snake.headCords[0] > this.appleCords[0]) {
+      console.log("COMPUTER SNAKE LEFT");
+      if (this.snake.direction !== RIGHT && !this.wouldCollideLeft(this.snake.headCords)) {
+        return LEFT;
+      } else if (!this.wouldCollideDown(this.snake.headCords)) {
+        return DOWN;
+      } else if (!this.wouldCollideUp(this.snake.headCords)) {
+        return UP;
+      }
+
+      // need to move right
+    } else if (this.snake.headCords[0] < this.appleCords[0]) {
+      console.log("COMPUTER SNAKE RIGHT");
+      if (this.snake.direction !== LEFT && !this.wouldCollideRight(this.snake.headCords)) {
+        return RIGHT;
+      } else if (!this.wouldCollideUp(this.snake.headCords)) {
+        return UP;
+      } else if (!this.wouldCollideDown(this.snake.headCords)) {
+        return DOWN;
+    }
+    // need to move down
+    } else if(this.snake.headCords[1] < this.appleCords[1]) {
+      console.log("COMPUTER SNAKE DOWN");
+      if (this.snake.direction !== UP && !this.wouldCollideDown(this.snake.headCords)) {
+        return DOWN;
+      } else if (!this.wouldCollideLeft(this.snake.headCords)) {
+        return LEFT;
+      } else if (!this.wouldCollideRight(this.snake.headCords)) {
+        return RIGHT;
+      }
+    }
+    else {
+      // do nothing
+    }
+    return this.snake.direction;
+  },
+  wouldCollide: function(headCords) {
+    for(let cords of this.snake.cords.slice(1)) {
+      if (headCords[0] === cords[0] && headCords[1] === cords[1]) {
+        return true
+      }
+    }
+    return false
+  },
+  wouldCollideLeft: function(headCords) {
+    console.log("checking left direction with head cords : " + headCords)
+    for(let cords of this.snake.cords.slice(1)) {
+      if (headCords[0] -SPEED  === cords[0] && headCords[1] === cords[1]) {
+        console.log("would collide left, avoiding direction!");
+        return true
+      }
+    }
+    return false
+  },
+  wouldCollideRight: function(headCords) {
+    console.log("checking right direction with head cords : " + headCords)
+    for(let cords of this.snake.cords.slice(1)) {
+      if (headCords[0] +SPEED === cords[0] && headCords[1] === cords[1]) {
+        console.log("would collide right, avoiding direction!");
+        return true
+      }
+    }
+    return false
+  },
+  wouldCollideUp: function(headCords) {
+    console.log("checking up direction with head cords : " + headCords)
+    for(let cords of this.snake.cords.slice(1)) {
+      if (headCords[0] === cords[0] && headCords[1] - SPEED === cords[1]) {
+        console.log("would collide up, avoiding direction!");
+        return true
+      }
+    }
+    return false
+  },
+  wouldCollideDown: function(headCords) {
+    console.log("checking down direction with head cords : " + headCords)
+    for(let cords of this.snake.cords.slice(1)) {
+      if (headCords[0]=== cords[0] && headCords[1] + SPEED === cords[1]) {
+        console.log("would collide down, avoiding direction!");
+        return true
+      }
+    }
+    return false
+  }
+
+}
+
 snake.init();
 apple.init();
+game.status = DEMO;
+if(game.status === DEMO) {
+  computerSnake.init(snake.init());
+}
 
 function draw() {
   ctx.clearRect(0, 0, WIDTH, HEIGHT);
@@ -176,8 +291,16 @@ function draw() {
   while(!apple.spawned && !game.safeAppleSpawn()) {
     apple.init();
   }
-  if(game.status === PLAY || game.status === PAUSED){
+  if(game.status === PLAY || game.status === PAUSED || game.status === DEMO){
     apple.draw();
+  }
+  if(game.status === DEMO) {
+    computerSnake.getAppleCords();
+    console.log("Computer snake tracking apple at x: " + computerSnake.appleCords[0] +
+    " y: " + computerSnake.appleCords[1]);
+    console.log("computer snake direction : " + computerSnake.snake.direction);
+    snake.direction = computerSnake.nextMove();
+    computerSnake.nextMove();
   }
   snake.draw();
   if (game.appleEaten()) {
@@ -189,14 +312,14 @@ function draw() {
     game.status = GAME_OVER;
     apple.spawned = false;
   }
-  // console.log(`x : ${snake.cords[0][0]}, y : ${snake.cords[0][1]} moving ${snake.direction}`);
+  console.log(`x : ${snake.cords[0][0]}, y : ${snake.cords[0][1]} moving ${snake.direction}`);
   // console.log(`GAME STATUS : ${game.status} KEY UP : ${game.keyup}`);
 
 }
 
 window.addEventListener("keydown", (event) => {
   // console.log(`key ${event.key}`);
-  if ((game.status === NEW_GAME || game.status === GAME_OVER) && event.key === NEW_GAME_KEY_CODE){
+  if ((game.status === NEW_GAME || game.status === GAME_OVER) && event.key === NEW_GAME_KEY_CODE) {
     snake.init();
     apple.init();
     game.status = PLAY;
@@ -206,7 +329,7 @@ window.addEventListener("keydown", (event) => {
     game.keyup = false;
     game.status = PLAY;
     snake.speed = SPEED;
-  } if (!snake.firstMove && game.status !== PAUSED) {
+  } if (!snake.firstMove && game.status !== PAUSED && game.status !== DEMO) {
     if (event.key === LEFT_KEY_CODE && snake.direction !== RIGHT && snake.direction !== LEFT) {
       snake.firstMove = true;
       snake.direction = LEFT;
@@ -219,7 +342,7 @@ window.addEventListener("keydown", (event) => {
     } else if (event.key === DOWN_KEY_CODE && snake.direction !== UP && snake.direction !== DOWN) {
       snake.firstMove = true;
       snake.direction = DOWN;
-    } else if (game.keyup && game.status === PLAY && event.key === PAUSE_TOGGLE_KEY_CODE) {
+    } else if (game.keyup && (game.status === PLAY || game.status === DEMO ) && event.key === PAUSE_TOGGLE_KEY_CODE) {
       game.keyup = false;
       snake.speed = 0;
       game.status = PAUSED;
@@ -231,5 +354,7 @@ window.addEventListener("keyup", (event) => {
     game.keyup = true;
   }
 })
+
+
 
 setInterval(draw, INTERVAL);
